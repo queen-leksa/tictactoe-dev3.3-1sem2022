31063740eed56b9ab0199b653aa52444f20a71a8
Leksa <surnacheva@ithub.ru>
2022-09-22 17:49:58 +0300

    update board size by input

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 import f from "./js/functions.js";
 const board = document.querySelector(".board");
+const statsEl = document.querySelector(".stats-box");
+const sliderEl = document.querySelector("[type=\"checkbox\"]");
+const sizeEl = document.querySelector("[type=\"number\"]");
+const btnEl = document.querySelector(".reset");
+
+
 const field = [];
 let n = 3;
 const chars = ["⮾", "⦾"];
 f.setField(n, board, field);
 let stepCnt = n ** 2;
 let step = 0;
+let startFlag = false;
+const statistics = [];
+
+sizeEl.addEventListener("input", (e) => {
+    n = +e.target.value;
+    if (!startFlag) {
+        f.setField(n, board, []);
+        statistics.push(`Выбрано поле ${n}x${n}`);
+        statsEl.innerHTML += `<div>${statistics[statistics.length - 1]}</div>`
+    }
+});
 
 
 // TODO: Создать поле ввода числа на основании которого рисуется поле для игры
