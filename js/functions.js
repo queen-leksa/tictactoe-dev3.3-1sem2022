11e06202724a40adb8817add8fb80dee3fd2dfb9
Leksa <surnacheva@ithub.ru>
2022-09-22 16:08:59 +0300

    upd html-content

--- js/functions.js
+++ js/functions.js
@@ -47,7 +47,6 @@ const setField = (n, board, field) => {
         board.innerHTML += `<div class="board__cell">${cell}</div>`;
     }
 }
-
 const isWin = function(field, step, n) {
     /*
         [0,1,2,3,4,5,6,7,8]
